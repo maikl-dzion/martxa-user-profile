49193db54e5f3273622a5eafaba32b09071f287f
Abasov Maikl <abasov.m@basis-plus.ru>
2021-03-24 11:34:23 +0300

    update users list page

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div @click="customDropdownClose" >
+  <div @click="customDropdownClose">
 
     <NewPreloader/>
 
@@ -24,7 +24,7 @@
     <!-- search-area -->
 
     <transition name="fade-app">
-       <router-view></router-view>
+      <router-view></router-view>
     </transition>
 
     <!--- Анимированные button (примеры) --->
@@ -42,133 +42,148 @@
     <!--      <span class="btn-hover-effect square-list" >ЭФФЕКТ 44</span>-->
     <!--    </p>-->
 
-    </div>
+  </div>
 </template>
 
-  <script>
+<script>
 
-  import NewPreloader from '@/components/app/NewPreloader'
-  import AlertMessage from '@/components/app/AlertMessage'
+import NewPreloader from '@/components/app/NewPreloader'
+import AlertMessage from '@/components/app/AlertMessage'
 
-  export default {
-    name: 'App',
-    data() {
-      return {
-        userId: 0,
-      }
-    },
+export default {
+  name: 'App',
+  data () {
+    return {
+      userId: 0,
+    }
+  },
 
-    created() {
-        this.userId = this.store('user_id')
-        this.setRootFilesPath()
-        this.setStoreUserInfo ();
-        this.fetchUsers();
-    },
+  created () {
+    this.userId = this.store('user_id')
+    this.setRootFilesPath()
+    this.setStoreUserInfo()
+    this.fetchUsers()
+  },
 
-    methods: {
-      customDropdownClose() {
-        const eventName = 'dropdown-menu-close';
-        this.sendEventBus (eventName, { status : false});
-      }
-    },
+  methods: {
+    customDropdownClose () {
+      const eventName = 'dropdown-menu-close'
+      this.sendEventBus(eventName, { status: false })
+    }
+  },
 
-    components : { NewPreloader, AlertMessage },
+  components: { NewPreloader, AlertMessage },
 
-    mounted() {
-      this.getEventBus('auth_event', resp => {
-        this.userId = resp.user_id
-      })
-    },
+  mounted () {
+    this.getEventBus('auth_event', resp => {
+      this.userId = resp.user_id
+    })
+  },
 
-  }
-  </script>
+}
+</script>
 
-  <style scoped>
+<style scoped>
 
-  .fade-app-enter-active, .fade-app-leave-active {
-    transition: opacity 0.2s;
-  }
-  .fade-app-enter, .fade-app-leave-to  {
-    opacity: 0.9;
-  }
+.fade-app-enter-active, .fade-app-leave-active {
+  transition: opacity 0.2s;
+}
 
-
-  /* Анимации появления и исчезновения могут иметь */
-  /* различные продолжительности и динамику.       */
-  /*.fade-appe-enter-active {*/
-  /*  transition: all .3s ease;*/
-  /*}*/
-  /*.fade-app-leave-active {*/
-  /*  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);*/
-  /*}*/
-  /*.fade-app-enter, .fade-app-leave-to {*/
-  /*  transform: translateX(10px);*/
-  /*  opacity: 0;*/
-  /*}*/
-
-  </style>
+.fade-app-enter, .fade-app-leave-to {
+  opacity: 0.9;
+}
 
 
-  <style>
+/* Анимации появления и исчезновения могут иметь */
+/* различные продолжительности и динамику.       */
+/*.fade-appe-enter-active {*/
+/*  transition: all .3s ease;*/
+/*}*/
+/*.fade-app-leave-active {*/
+/*  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);*/
+/*}*/
+/*.fade-app-enter, .fade-app-leave-to {*/
+/*  transform: translateX(10px);*/
+/*  opacity: 0;*/
+/*}*/
 
-  * {
-    box-sizing: border-box;
-    font-size: 12px;
-  }
-
-  button {
-     cursor: pointer;
-  }
-
-  input {
-    cursor: pointer;
-  }
-
-  /***** КНОПКИ  *****/
-
-  .span-shadow {text-align:center}
-  .span-shadow .btn-hover-effect {
-    display: inline-block;
-    margin: 5px;
-    padding: 7px 15px;
-    cursor: pointer;
-    border-radius: 0px;
-    transition: .2s linear;
-    color: white;
-    background:#337ab7;
-  }
-
-  .span-shadow .square-out {background:#3A2F28}
-  .span-shadow .square-out:hover {
-    box-shadow: 0 0 0 2px white, 0 0 0 4px #3A2F28;
-  }
-
-  .span-shadow .shadow-live {background:#C1F1E4}
-  .span-shadow .shadow-live:hover {
-    box-shadow: 200px 0 0 0 rgba(0,0,0,.3) inset;
-  }
-
-  .span-shadow .square-in {background:#D2973D}
-  .span-shadow .square-in:hover {
-    box-shadow: 0 0 0 2px #D2973D inset, 0 0 0 4px white inset;
-  }
-
-  .span-shadow .square-list {
-    background: #C76637;
-    position: relative;
-    top: 0;
-    left: 0;
-  }
-  .span-shadow .square-list:hover {
-    box-shadow: 2px 2px white, 4px 4px #C76637;
-    top: -4px;
-    left: -4px;
-  }
-  /****  .КНОПКИ  *****/
-
-  .span-shadow .square-in:hover {
-    box-shadow: 0 0 0 2px #1946BA inset, 0 0 0 4px white inset;
-  }
+</style>
 
 
-  </style>
+<style>
+
+* {
+  box-sizing: border-box;
+  font-size: 12px;
+}
+
+button {
+  cursor: pointer;
+}
+
+input {
+  cursor: pointer;
+}
+
+/***** КНОПКИ  *****/
+
+.span-shadow {
+  text-align: center
+}
+
+.span-shadow .btn-hover-effect {
+  display: inline-block;
+  margin: 5px;
+  padding: 7px 15px;
+  cursor: pointer;
+  border-radius: 0px;
+  transition: .2s linear;
+  color: white;
+  background: #337ab7;
+}
+
+.span-shadow .square-out {
+  background: #3A2F28
+}
+
+.span-shadow .square-out:hover {
+  box-shadow: 0 0 0 2px white, 0 0 0 4px #3A2F28;
+}
+
+.span-shadow .shadow-live {
+  background: #C1F1E4
+}
+
+.span-shadow .shadow-live:hover {
+  box-shadow: 200px 0 0 0 rgba(0, 0, 0, .3) inset;
+}
+
+.span-shadow .square-in {
+  background: #D2973D
+}
+
+.span-shadow .square-in:hover {
+  box-shadow: 0 0 0 2px #D2973D inset, 0 0 0 4px white inset;
+}
+
+.span-shadow .square-list {
+  background: #C76637;
+  position: relative;
+  top: 0;
+  left: 0;
+}
+
+.span-shadow .square-list:hover {
+  box-shadow: 2px 2px white, 4px 4px #C76637;
+  top: -4px;
+  left: -4px;
+}
+
+/****  .КНОПКИ  *****/
+
+.span-shadow .square-in:hover {
+  box-shadow: 0 0 0 2px #1946BA inset, 0 0 0 4px white inset;
+}
+
+</style>
+
--- src/pages/UsersList.vue
+++ src/pages/UsersList.vue
@@ -1,56 +1,125 @@
-<template>
-  <page-template>
+<template><page-template >
+<section class="blog-details-area1 ptb-101" style="border:0px red solid; ">
+  <div style="border:0px red solid;">
+      <h3 class="sidebar-title" style="background: #296dc1; padding: 15px; color:white;">Список пользователей</h3>
+      <div class="row" style="background: #296dc1; padding: 0px; margin: 0px;">
 
-    <!-- blog-details-area start -->
-    <section class="blog-details-area1 ptb-101" style="border:0px red solid; ">
+            <!--- ЛЕВАЯ ПАНЕЛЬ ---->
+            <div class="col-md-6 col-sm-6 col-xs-12" style="border:0px orange solid; padding:4px" >
+              <aside class="left-sidebar" style="padding: 0px" >
 
-      <div style="border:0px red solid;">
+<!--                <div class="mb-20 about-wrap user-menu user-profile-menu">-->
+<!--                  <ul><li v-for="(user) in getUsers" :key="user.user_id" >-->
+<!--  -->
+<!--                      <div style="display: flex; margin-top: 20px;">-->
+<!--                          <img :src="user.avatar" style="width:70px; margin-right: 2px;">-->
+<!--                          <div @click="selectedUser($event, user)" class="user-list-card" style="width:100%; border: 1px white solid" >-->
+<!--                              <div>{{user.username}}</div>-->
+<!--                              <div>{{user.role_name}}</div>-->
+<!--                          </div>-->
+<!--                      </div>-->
+<!--  -->
+<!--                  </li></ul>-->
+<!--                </div>-->
 
-        <h3 class="sidebar-title" style="background: #296dc1; padding: 15px; color:white;">Список пользователей</h3>
+                <section  v-for="(user) in getUsers" :key="user.user_id"
+                          class="section section-about wow fadeInUp wt-sky-reading-list-page"
+                          style="visibility: visible; animation-name: fadeInUp; padding:3px; margin-bottom: 10px;" >
+                          <div @click="selectedUser($event, user)" class="profile" style="cursor: pointer"><div class="row">
 
-        <div class="row" style="background: #296dc1; padding: 0px; margin: 0px;">
+                              <div class="col-sm-4">
+                                <div class="photo-profile">
+                                    <img :src="user.avatar" >
+                                </div>
 
-<!--          <pre>{{getUsers}}</pre>-->
+<!--                                <a href="https://rstill.netlify.app/cv/cv-1.pdf" target="cv">-->
+<!--                                  <div class="download-resume">-->
+<!--                                    <i class="fa fa-cloud-download" aria-hidden="true"></i>-->
+<!--                                    <span class="text-download">DOWNLOAD RESUME</span>-->
+<!--                                  </div>-->
+<!--                                </a>-->
+<!--                                -->
+<!--                                <div class="available">-->
+<!--&lt;!&ndash;                                  <i class="fa fa-check" aria-hidden="true"></i>&ndash;&gt;-->
+<!--                                  <span class="text-available">{{user.role_name}}</span>-->
+<!--                                </div>-->
 
-          <!--- ЛЕВАЯ ПАНЕЛЬ ---->
-          <div class="col-md-6 col-sm-6 col-xs-12" style="border:0px orange solid; padding:4px 10px 10px 10px" >
-            <aside class="left-sidebar" style="padding: 0px" >
+                              </div>
 
-              <div class="mb-20 about-wrap user-menu user-profile-menu">
-                <ul><li v-for="(user) in getUsers" :key="user.user_id" >
+                              <div class="col-sm-8">
+                                <div class="info-profile">
 
-                    <div style="display: flex; margin-top: 20px;">
-                        <img :src="user.avatar" style="width:70px; margin-right: 2px;">
-                        <div @click="selectedUser($event, user)" class="user-list-card" style="width:100%; border: 1px white solid" >
-                            <div>{{user.username}}</div>
-                            <div>{{user.role_name}}</div>
-                        </div>
-                    </div>
+                                  <h4><span></span> {{user.username}} </h4>
+                                  <h6><span>Роль : </span> {{user.role_name}} </h6>
+<!--                                  <h6>web developer &amp; web designer</h6>-->
+                                  <div class="row">
 
-                </li></ul>
-              </div>
+                                    <div class="col-sm-6">
+                                      <ul class="ul-info">
+                                        <li class="li-info">
+                                          <span class="title-info">Дата рождения : </span>
+                                          <span class="info">{{user.date_of_birth}}</span>
+                                        </li>
+                                        <li class="li-info">
+                                          <span class="title-info">Адрес : </span>
+                                          <span class="info">{{user.address}}</span>
+                                        </li>
+                                        <li class="li-info">
+                                          <span class="title-info">Email : </span>
+                                          <span class="info">{{user.email}}</span>
+                                        </li>
+                                      </ul>
+                                    </div>
 
-            </aside>
-          </div>
-          <!--- ПРАВАЯ ПАНЕЛЬ ---->
-          <div class="col-md-6 col-sm-6 col-xs-12"
-               style="background: white; border: 1px #296dc1 solid; height: 100%" >
-              <div class="blog-details-wrap" style="padding-top:20px;" >
+                                    <div class="col-sm-6">
+                                      <ul class="ul-info">
+                                        <li class="li-info">
+                                          <span class="title-info">Телефон : </span>
+                                          <span class="info">{{user.pnone}}</span>
+                                        </li>
+                                        <li class="li-info">
+                                          <span class="title-info">Website : </span>
+                                          <span class="info"></span>
+                                        </li>
+                                        <li class="li-info">
+                                          <span class="title-info"> Национальность : </span>
+                                          <span class="info"></span>
+                                        </li>
+                                      </ul>
+                                    </div>
 
-                <template v-if="tab == 'users_list'">
-                  <UserInfoEditForm :user="selectUser"/>
-                </template>
+                                  </div>
 
-              </div>
-          </div>
+                                  <hr/>
+                                  <p> Короткое описание о себе : </p>
+                                  <p>  {{user.note}} </p>
 
-        </div>
-      </div>
-    </section>
-    <!-- blog-details-area end -->
+                                </div>
+                              </div>
 
-  </page-template>
-</template>
+                          </div></div><!-- .profile .row -->
+                </section>
+
+              </aside>
+            </div>
+
+            <!--- ПРАВАЯ ПАНЕЛЬ ---->
+            <div class="col-md-6 col-sm-6 col-xs-12"
+                 style="background: white; border: 1px #296dc1 solid; height: 100%" >
+                <div class="blog-details-wrap" style="padding-top:20px;" >
+
+                  <template v-if="tab == 'users_list'">
+                     <pre>{{selectUser}}</pre>
+<!--                     <UserInfoEditForm :user="selectUser"/>-->
+                  </template>
+
+                </div>
+            </div>
+
+      </div><!-- .row -->
+  </div>
+</section>
+</page-template></template>
 
 <script>
 
@@ -64,25 +133,25 @@ import MediaManager from '@/components/app/MediaManager'
 export default {
 
   data: () => ({
-    tab: 'users_list',
-    emailVerifyCode: null,
-    emailVerifyState: false,
-    phoneVerifyState: false,
-    newPassword: {
-      password: '',
-      repeat_pwd: ''
-    },
-    userFiles : [],
-    usersList : [],
-    fileAlbums: {},
-    selectUser : {},
+      tab: 'users_list',
+      emailVerifyCode: null,
+      emailVerifyState: false,
+      phoneVerifyState: false,
+      newPassword: {
+        password: '',
+        repeat_pwd: ''
+      },
+      userFiles : [],
+      usersList : [],
+      fileAlbums: {},
+      selectUser : {},
   }),
 
   components: {
-    UserInfoEditForm,
-    MultiFilesUploader,
-    UserAvatar,
-    MediaManager
+      UserInfoEditForm,
+      MultiFilesUploader,
+      UserAvatar,
+      MediaManager
   },
 
   created () {
@@ -112,51 +181,6 @@ export default {
       this.saveResponse(response, 'Файлы успешно загружены', 'Не удалось загрузить файлы')
     },
 
-    deleteFile (fileId) {
-      this.setPreloader(true)
-      const apiUrl = '/delete/file/' + fileId
-      this.send(apiUrl, 'delete').then(response => {
-        this.getUserFiles()
-        this.saveResponse(response, 'Файл удален', 'Файл не получилось удалить')
-      })
-    },
-
-    compareNewPassword () {
-      if (this.newPassword.password != this.newPassword.repeat_pwd) {
-        return 'Пароли не совпадают'
-      }
-      return ''
-    },
-
-    changePassword () {
-      if (!this.changePasswordValidate()) return false
-      const data = this.newPassword
-      const apiUrl = '/post/user/change-password/' + this.userId
-      this.setPreloader(true)
-      this.send(apiUrl, 'put', data).then(response => {
-        this.saveResponse(response, 'Пароль успешно изменен', 'Не удалось изменить пароль')
-      })
-    },
-
-    changePasswordValidate () {
-      if (!this.newPassword.password) {
-        return false
-      }
-      let message = this.compareNewPassword()
-      if (message) {
-        this.responseMessage = message
-        this.responseColor = 'red'
-        return false
-      }
-      this.respInfoClear()
-      return true
-    },
-
-    responseStatusHandle (response, successMessage = null, errorMessage = null, fn = null) {
-      const res = this.saveResponse(response, successMessage, errorMessage)
-      if (fn) fn(res)
-      return true
-    },
 
     getUserFiles () {
       const apiUrl = '/user/get-files/' + this.userId
@@ -176,19 +200,19 @@ export default {
     },
 
     selectedUser(event, user){
-      const activeClass = 'user-menu-active';
-      let aMenuList = document.querySelectorAll('.user-profile-menu li div');
-      this.htmlElemsRender(aMenuList, (elem) => {
-        if(elem && elem.classList)
-          elem.classList.remove(activeClass)
-      });
-      let target = event.target;
-      const cl = target.classList.contains('user-list-card')
-      if(!cl) {
-        target = event.target.parentNode;
-      }
-      target.classList.add(activeClass);
-      this.selectUser = user;
+        // const activeClass = 'user-menu-active';
+        // let aMenuList = document.querySelectorAll('.user-profile-menu li div');
+        // this.htmlElemsRender(aMenuList, (elem) => {
+        //   if(elem && elem.classList)
+        //     elem.classList.remove(activeClass)
+        // });
+        // let target = event.target;
+        // const cl = target.classList.contains('user-list-card')
+        // if(!cl) {
+        //   target = event.target.parentNode;
+        // }
+        // target.classList.add(activeClass);
+        this.selectUser = user;
     },
 
     async getUserAvatar(userId) {
